d0961ee3281bf92dc489873c5963a8425e66d753
JacobMennell <jakemennell@hotmail.co.uk>
2022-11-11 17:30:47 +0000

    minor refactor for efficiency

--- data_collection.py
+++ data_collection.py
@@ -6,6 +6,7 @@ from gather_keys_oauth2 import OAuth2Server
 import os
 import json
 
+
 class FitbitAnalysis:
 
     def __init__(self, client_id: str, client_secret: str):
@@ -13,21 +14,27 @@ class FitbitAnalysis:
         self.client_secret = client_secret
 
         # create a FitbitOauth2Client object.
-        server = OAuth2Server(self.client_id,self.client_secret)
+        server = OAuth2Server(self.client_id, self.client_secret)
         server.browser_authorize()
         ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
         REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
 
         self.fit = fitbit.Fitbit(client_id, client_secret, oauth2=True, access_token=ACCESS_TOKEN,
-                            refresh_token=REFRESH_TOKEN)
-
+                                 refresh_token=REFRESH_TOKEN)
 
     def get_x_days_activity(self, no_days_ago: int) -> pd.DataFrame:
+        """_summary_
+
+        Args:
+            no_days_ago (int): days of data to return
+
+        Returns:
+            pd.DataFrame: activity data frame
+        """
 
         # define last 10 days of data
-        days_list = []
-        for i in range(1, no_days_ago+1):
-            days_list.append(str((datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%Y-%m-%d")))
+        days_list = [str((datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%Y-%m-%d")) for i in
+                     range(1, no_days_ago + 1)]
 
         # variable list
         id_list = []
@@ -47,11 +54,11 @@ class FitbitAnalysis:
 
         # make DataFrame
         activity_df = pd.DataFrame({'id': id_list,
-                                   'Name': name_list,
-                                   'Start_Date': date_list,
-                                   'Calories': calories_list,
-                                   'Steps': steps_list
-                                   })
+                                    'Name': name_list,
+                                    'Start_Date': date_list,
+                                    'Calories': calories_list,
+                                    'Steps': steps_list
+                                    })
 
         # cast data types
         activity_df['id'] = activity_df['id'].astype(int)
@@ -63,27 +70,32 @@ class FitbitAnalysis:
         # return DataFrame
         return activity_df
 
-
     def get_x_days_sleep(self, no_days_ago: int) -> pd.DataFrame:
+        """_summary_
+
+        Args:
+            no_days_ago (int): days of data to return
+
+        Returns:
+            pd.DataFrame: sleep data frame
+        """
 
         # define last 10 days of data
-        days_list = []
-        for i in range(1, no_days_ago + 1):
-            days_list.append(str((datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%Y-%m-%d")))
+        days_list = [str((datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%Y-%m-%d")) for i in
+                     range(1, no_days_ago + 1)]
 
         # get activities for last x days
         sleep_time_list = []
         sleep_val_list = []
+
         for day in days_list:
             sleep_func = self.fit.sleep(date=day)
             for i in sleep_func['sleep'][0]['minuteData']:
                 sleep_time_list.append(i['dateTime'])
                 sleep_val_list.append(i['value'])
 
-        sleep_df = pd.DataFrame({'State': sleep_val_list,
-                                 'Time': sleep_time_list})
+        return pd.DataFrame({'State': sleep_val_list, 'Time': sleep_time_list})
 
-        return sleep_df
 
 # get credentials for api and google sheet source
 with open('cred.json') as data_file:
