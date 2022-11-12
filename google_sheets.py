5499764ed2771f61c7ffc8026ef9fd9a7d3f83e0
JacobMennell <jakemennell@hotmail.co.uk>
2022-11-12 11:36:35 +0000

    removed unused code

--- google_sheets.py
+++ google_sheets.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import os
-# import pickl
-# used to access google drive
 import gspread as gs
 
 
@@ -24,48 +21,3 @@ def get_google_sheet(sheet_url: str, sheet_name: str) -> pd.DataFrame:
     df = df.applymap(lambda s: s.upper() if type(s) == str else s)
 
     return df
-
-# using sheet api waiting for approved scope
-# from googleapiclient.discovery import build
-# from google_auth_oauthlib.flow import InstalledAppFlow,Flow
-# from google.auth.transport.requests import Request
-# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-#
-# # set the environment variables
-# SHEET_ID = os.getenv('SHEET_ID')
-# SHEET_RANGE = 'A1:F14'
-#
-#
-# def sheet_load():
-#     '''
-#     '''
-#     global values_input, service
-#     creds = None
-#     if os.path.exists('token.pickle'):
-#         with open('token.pickle', 'rb') as token:
-#             creds = pickle.load(token)
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(
-#                 'my_json_file.json', SCOPES) # here enter the name of your downloaded JSON file
-#             creds = flow.run_local_server(port=0)
-#         with open('token.pickle', 'wb') as token:
-#             pickle.dump(creds, token)
-#
-#     service = build('sheets', 'v4', credentials=creds)
-#
-#     # Call the Sheets API
-#     sheet = service.spreadsheets()
-#     result_input = sheet.values().get(spreadsheetId=SHEET_ID,
-#                                 range=SHEET_RANGE).execute()
-#     values_input = result_input.get('values', [])
-#
-#     if not values_input and not values_expansion:
-#         print('No data found.')
-#
-#     return pd.DataFrame(values_input[1:], columns=values_input[0])
-#
-#
-# df = sheet_load()
