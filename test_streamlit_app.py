9f55dbc71ec9fa70ad34c9a8b76103a358abfaca
JacobMennell <jakemennell@hotmail.co.uk>
2022-11-14 08:38:49 +0000

    clean up pytests with fixtures

--- test_streamlit_app.py
+++ test_streamlit_app.py
@@ -2,10 +2,11 @@ import pandas as pd
 import pytest
 import json
 from google_sheets import get_google_sheet
-#from data_collection import FitbitAnalysis
+from data_collection import FitbitAnalysis
 import warnings
 
-SCOPE="function"
+# define scope for pytest.fixture
+scope = 'session'
 
 def api_v1():
     warnings.warn(UserWarning("api v1, should use functions from v2"))
@@ -15,12 +16,23 @@ def api_v1():
 def test_one():
     assert api_v1() == 1
 
-
-def test_get_google_sheet():
+@pytest.fixture(scope=scope)
+def get_cred():
     with open('cred.json') as data_file:
         data = json.load(data_file)
+    return data
 
-    result = get_google_sheet(data['sheet_url'], 'PB')
+
+@pytest.fixture(scope=scope)
+def shared_instance(get_cred):
+
+    fitinst = FitbitAnalysis(get_cred['client_id'], get_cred['client_secret'])
+    yield fitinst
+
+
+def test_get_google_sheet(get_cred):
+
+    result = get_google_sheet(get_cred['sheet_url'], 'PB')
 
     #  test datatype
     assert isinstance(result, pd.DataFrame)
@@ -28,14 +40,10 @@ def test_get_google_sheet():
     # test columns
     assert len(result.axes[1]) == 4
 
-@pytest.fixture(scope='function')
-def shared_instance():
-    fitinst = FitbitAnalysis(data['client_id'], data['client_secret'])
-    yield fitinst
 
 def test_get_x_days_activity(shared_instance):
-    fitinst = FitbitAnalysis(data['client_id'], data['client_secret'])
-    result = fitinst.get_x_days_activity(10, client_id, client_secret)
+
+    result = shared_instance.get_x_days_activity(10)
 
     #  test datatype
     assert isinstance(result, pd.DataFrame)
