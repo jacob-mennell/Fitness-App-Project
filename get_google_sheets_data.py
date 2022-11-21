3f198631809d1af1531add7c9dc32b04b64415aa
JacobMennell <jakemennell@hotmail.co.uk>
2022-11-21 18:27:16 +0000

    documentation

--- get_google_sheets_data.py
+++ get_google_sheets_data.py
@@ -36,6 +36,12 @@ def export_to_google_sheets(sheet_url: str,
                             sheet_name: str,
                             df_new: pd.DataFrame,
                             credentials: dict) -> None:
+    """
+    :param sheet_url: str
+    :param sheet_name: str
+    :param df_new: pd.DataFrame
+    :param credentials: google sheet credentials from service_account.jso
+    """
     # authenticate
     gc = gs.service_account_from_dict(credentials)
 
@@ -45,13 +51,16 @@ def export_to_google_sheets(sheet_url: str,
     # select workbook
     sheet = sh.worksheet(sheet_name)
 
-    # add rows from fdf
+    # add rows from df
     df_current = get_google_sheet(sheet_url, sheet_name, credentials)
 
+    # concat new data with existing
     final_df = pd.concat([df_current, df_new], join="inner")
 
+    # clear data
     sheet.clear()
 
+    # add new concat final_df_data
     gd.set_with_dataframe(worksheet=sheet,
                           dataframe=final_df,
                           include_index=False,
